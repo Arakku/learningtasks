043c9d9751da6491ff4d83e3b6bf5fe041588961
Arakku <100070895+Arakku@users.noreply.github.com>
2022-03-14 22:35:25 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!Doctype html>
+<html>
+<head>
+ <title> My Recipe Homepage </title>
+ </head>
+ 
+ <body>
+ <center><h1> Welcome to my Webpage. Hi! my name if Gideon Calderon BSIT 1D</h1></center>
+ <ul>
+ <li><a href="index.php">Home</a></li>
+ <li><a href="Top 10 Celebrities.html"> LEARNING TASK 3 </a></li>
+ </ul>
+ 
+ </body>
+</html>
