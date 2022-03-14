f46943f769f579d30d90f2188e95ab22719ba5c0
Arakku <100070895+Arakku@users.noreply.github.com>
2022-03-14 22:18:51 +0800

    Create index.php

---
+++ Websites/index.php
@@ -0,0 +1,15 @@
+<!Doctype html>
+<html>
+<head>
+ <title> My Recipe Homepage </title>
+ </head>
+ 
+ <body>
+ <center><h1> Welcome to my Webpage. Hi! my name if Gideon Calderon</h1></center>
+ <ul>
+ <li><a href="index.php">Home</a></li>
+ <li><a href="https://github.com/GidCalds/learningtasks.git"> LEARNING TASK 3 </a></li>
+ </ul>
+ 
+ </body>
+</html>
