0ef0e07026f8f680c7908affaef246a1514182d0
Arakku <100070895+Arakku@users.noreply.github.com>
2022-03-24 22:11:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
  <center><h1> Welcome to my Webpage. Hi! my name if Gideon Calderon BSIT 1D</h1></center>
  <ul>
  <li><a href="index.php">Home</a></li>
+ <li><a href="The Recipe Website.html"> LEARNING TASK 2 </a></li>
  <li><a href="Top 10 Celebrities.html"> LEARNING TASK 3 </a></li>
+ <li> <a href="CLE1MT/Poverty Website.html"> MIDTERM </a> </li>
+  
  </ul>
  
  </body>
